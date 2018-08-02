69cc8481d19bf958dd1c3dc5759f1e60b5897848
Bunlong <bunlong.van@gmail.com>
2018-08-03 00:20:18 +0700

    ??????? finished setting up react env from scratch  ???????

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+  return (
+    <div>
+      <p>Hello React from The Modern Javascript!</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+  }
+};
